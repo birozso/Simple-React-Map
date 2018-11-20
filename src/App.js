72fe5c0a707ccfd9de7cf3c982b7447c84b69884
csenge <csengeweb@gmail.com>
2018-11-20 13:28:58 +0100

    added  prop-type, bug: display markers:

--- src/App.js
+++ src/App.js
@@ -5,44 +5,88 @@ import axios from 'axios';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import MapContainer from './components/MapContainer';
+import PropTypes from 'prop-types';
+
+//import MapContainer from './components/MapContainer';
 
 
 class App extends Component {
- componentDidMount (){
-   this.getLocals()
- }
+
   
-  // Get Venues
-  getLocals = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/search?"
-    const parameters = {
-      client_id: "FTPQMQKRBNIJJDPKNGWFMUHD3KBP1OIYX0YZ5BU250CILCD4",
-      client_secret: "EZ3ACLWE1RBXRJSHLQCE0RU4DIYJTQB1SOEVVIK10OFKCR1F",
-      query: "cafe",
-      //ll: "48.208416, 16.37347",
-      near: "Vienna",
-      v: "20181108"
+
+ state = {
+      venues: [], 
     }
 
-    axios.get(endPoint + new URLSearchParams(parameters))
-      .then(response => {
-        this.setState({
-          venues: response.data.response.venues
-         })
-        //console.log(response.data.response.venues)
-      })
-      .catch(error => {
-        console.log("Sorry, there's an ERROR during at axios:-( " + error)
-      })
-    }
+
+componentDidMount = () => {
+  this.getLocals()
+  // Connect the initMap() function within this class to the global window context,
+  // so Google Maps can invoke it
+  window.initMap = this.initMap;
+  // Asynchronously load the Google Maps script, passing in the callback reference
+  loadJS('https://maps.googleapis.com/maps/api/js?key=AIzaSyCLVCGUR9jUVe3DkVKspecXg0pCgMK1E1M&callback=initMap')
+  //Get locals
+ 
+}
+
+
+ // Get Venues
+ getLocals = () => {
+  const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+  const parameters = {
+    client_id: "FTPQMQKRBNIJJDPKNGWFMUHD3KBP1OIYX0YZ5BU250CILCD4",
+    client_secret: "EZ3ACLWE1RBXRJSHLQCE0RU4DIYJTQB1SOEVVIK10OFKCR1F",
+    query: "cafe",
+    //ll: "48.208416, 16.37347",
+    near: "Vienna",
+    v: "20181108"
+  }
+
+  axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {console.log (response.data.response.groups[0].items)
+      this.setState({
+        //at search? the value of venues are: venues: response.data.response.venues
+        venues:response.data.response.groups[0].items
+
+      },  this.initMap()) 
+      //console.log(response.data.response.venues)
+    })
+    .catch(error => {
+      console.log("Sorry, there's an ERROR during at axios:-( " + error)
+    })
+  }
+
+// Initialize and add the map
+initMap = () => {
+
+  
+    // The map, centered at Vienna
+  var map = new window.google.maps.Map(
+      document.getElementById('map'), 
+      {zoom: 14, 
+        center: {lat: 48.208418, lng: 16.373231}
+      });
+
+
+  this.state.venues.map(singleVenue =>{
+   
+      let marker = new window.google.maps.Marker({
+      position: {lat: singleVenue.venue.location.lat, lng: singleVenue.venue.location.lng},
+      map: map,
+      title:"Hi Dude"
+    })
+    return marker;
+  })
+  
+} 
 
   render() {
     return (
       <div className = 'app'>
         
         <Header />
-        <MapContainer />
+        <div id='map'></div>
         <Footer />
       </div>
     );
@@ -50,3 +94,18 @@ class App extends Component {
 }
 
 export default App;
+function loadJS(src) {
+  var ref = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  ref.parentNode.insertBefore(script, ref);
+  
+  //TODO: add errormessage
+}
+
+
+
+App.propTypes = {
+  venues: PropTypes.array.isRequired
+}
